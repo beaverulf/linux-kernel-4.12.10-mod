3346d5905434b3d4a0ab48c7af9dd2cdbec89d98
lidell <lidell@cs.umu.se>
2017-08-31 23:13:18 +0200

    added to other syscall_64.tbl

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -205,7 +205,6 @@ extern struct trace_event_functions exit_syscall_print_funcs;
 		return ret;						\
 	}								\
 	static inline long SYSC##name(__MAP(x,__SC_DECL,__VA_ARGS__))
-asmlinkage long sys_icarus(void);
 asmlinkage long sys32_quotactl(unsigned int cmd, const char __user *special,
 			       qid_t id, void __user *addr);
 asmlinkage long sys_time(time_t __user *tloc);
@@ -905,5 +904,8 @@ asmlinkage long sys_pkey_alloc(unsigned long flags, unsigned long init_val);
 asmlinkage long sys_pkey_free(int pkey);
 asmlinkage long sys_statx(int dfd, const char __user *path, unsigned flags,
 			  unsigned mask, struct statx __user *buffer);
+
+
+asmlinkage long sys_icarus(void);
 
 #endif
