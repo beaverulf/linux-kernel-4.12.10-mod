6bcf7071df4bbcf6c182d0f031fd6c430e4bdce2
Your Name <you@example.com>
2017-09-01 14:57:19 +0100

    added user struct and make file changes

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo make -j 5 KDEB_PKGVERSION=1.icarus-kernel deb-pkg
+make -j 5 KDEB_PKGVERSION=1.icarus-kernel deb-pkg
--- icarus/icarus.c
+++ icarus/icarus.c
@@ -1,6 +1,12 @@
 #include <linux/kernel.h>
+#include <linux/cred.h>
+#include <linux/sched/user.h>
 
 asmlinkage long sys_icarus(void){
-	printk("Too close to the sun\n");
+
+	struct user_struct *user;
+	user = get_current_user();
+
+	printk("Number of processes: %d",user->__count);
 	return 0;
 }
