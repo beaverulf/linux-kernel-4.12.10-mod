b2d8778faac9554e305c3f889834d69fd189f465
Björn <bjorn.lidell@gmail.com>
2017-09-05 11:18:28 +0200

    updated icarus.c
    removed excess code

--- icarus/icarus.c
+++ icarus/icarus.c
@@ -14,9 +14,5 @@ asmlinkage long sys_icarus(
 	*sigpending = user->sigpending.counter;
 	*epoll_watches = user->epoll_watches.counter;
 	
-//	memcpy(processes,user->processes.counter,sizeof(int));
-//	memcpy(sigpending,user->sigpending.counter,sizeof(int));
-//	memcpy(epoll_watches,user->epoll_watches.counter,sizeof(long));
-	printk("# Processes: %d\n# Pending Signales %d\nEpoll watches: %ld\n",user->processes.counter,user->sigpending.counter,user->epoll_watches.counter);
 	return 0;
 }
